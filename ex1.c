95a95b3900657a0473a4bdab6f603bf218b3a605
Ali-ai62 <qaisturi14444@gmail.com>
2025-10-28 21:45:58 +0500

    Update ex1.c
    Qais Ali.
    Project 1
    Number guessing Game

--- ex1.c
+++ ex1.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main(void)
+{
+    int guess;
+    int secret = 3;
+
+    printf("Guess a number between 1 and 5: ");
+    scanf("%d", &guess);
+
+    if (guess == secret)
+    {
+        printf("You got it right!\n");
+    }
+    else
+    {
+        printf("Wrong! The correct number was %d.\n", secret);
+    }
+
+    return 0;
+}

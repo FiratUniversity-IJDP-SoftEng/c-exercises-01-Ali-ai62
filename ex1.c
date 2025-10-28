5e55cf5f9da31e606a9dd76e34c77e76641d1eb1
Ali-ai62 <qaisturi14444@gmail.com>
2025-10-28 21:52:18 +0500

    Update ex1.c
    Qais Ali
    Exercise 1-6

--- ex1.c
+++ ex1.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int age;
-    printf("Enter your age: ");
-    scanf("%d", &age);
+    char pet[10];  // Array to store pet type
 
-    for (int i = 0; i < age; i++)
+    printf("Do you have a cat or a dog? ");
+    scanf("%s", pet);  // Get string input
+
+    if (strcmp(pet, "cat") == 0)
     {
-        printf("Happy Birthday!\n");
+        printf("Meow!\n");
+    }
+    else if (strcmp(pet, "dog") == 0)
+    {
+        printf("Woof!\n");
+    }
+    else
+    {
+        printf("I don't know that pet.\n");
     }
 
     return 0;
